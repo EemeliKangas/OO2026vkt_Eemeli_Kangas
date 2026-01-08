89ea725c1f8ab65f5507da003cdbd09246c26bcd
Eemeli <e.kangas04@gmail.com>
2026-01-08 10:13:22 +0200

    Week 1 project ready

--- Viikko 1/main.cpp
+++ Viikko 1/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
+int game(int);
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int maxNum = 0;
+
+    cout << "Anna maksimi luku:";
+    cin >> maxNum;
+
+    game(maxNum);
+
 }
+int game(int maxNum){
+    srand(time(0));
+
+    int rNum = rand() % (maxNum+1);
+    int gNum = 0;
+    int numGuess = 0;
+
+
+
+    while(gNum!=rNum)
+    {
+        cout << "Arvaa luku valilta 1-"<<maxNum<<":";
+
+        cin >> gNum;
+        ++numGuess;
+
+        if(gNum>rNum)
+        {
+            cout << "Arvaus oli liian suuri!"<<endl;
+        }
+
+        else if(gNum<rNum)
+        {
+            cout << "Arvaus oli liian pieni!"<<endl;
+        }
+    }
+    cout << "Arvaus oli oikein!!"<<" Sait oikean numeron arvauskerralla:"<<numGuess<<endl;
+
+    return numGuess;
+}
+
